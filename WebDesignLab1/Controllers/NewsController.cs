7b0588c9391954a0bc6ba0d28688a915567a112f
Vitaliy Ugera <shnobel12@gmail.com>
2022-11-12 15:26:56 +0200

    Add project files.

---
+++ WebDesignLab1/Controllers/NewsController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebDesignLab1.Controllers
+{
+    public class NewsController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Contacts()
+        {
+            return View();
+        }
+
+        public IActionResult Topics()
+        {
+            return View();
+        }
+
+        public IActionResult Search()
+        {
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            return View();
+        }
+    }
+}
